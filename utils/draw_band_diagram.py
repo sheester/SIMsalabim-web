2fe8a5fafd902a40f7e18924825665b7d32af4d7
sheester <sander.heester@gmail.com>
2022-12-13 17:39:50 +0100

    Reworked Plot Function. Added width bar.

--- utils/draw_band_diagram.py
+++ utils/draw_band_diagram.py
@@ -1,64 +1,101 @@
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
-def create_rectangle_patch(
-        CB,
-        VB,
-        energy_offset,
-        energy_step_size,
-        p_origin,
-        width,
-        total_width_step_size,
-        color,
-        size,
-        ax):
-    # Description:  Draw a rectangle box representing a device layer.
-    # Arguments:    CB (float) -                    Conduction band edge or Work function for electrodes
-    #               VB (float) -                    Valence band edge or Work function for electrodes
-    #               energy_offset (float) -         Energy offset to shift diagram in place (=maximum energy)
-    #               energy_step_size (float) -      Amount of eV per grid point
-    #               p_origin (float) -              x location of the previous layer
-    #               width (float) -                 Width of the layer
-    #               total_width_step_size (float) - Amount of m per grid point
-    #               color (string) -                Outline color of layer
-    #               size (number) -                 Number of grid points
-    #               ax (ax) -                       Figure
-    # Returns:      rect (Rectangle) -              Rectangle patches object.
-    #               p_right (float) -               x value of right border of current layer to be used as input for x position of the next layer
-    e_CB = (CB-energy_offset)/energy_step_size
-    e_VB = (VB-energy_offset)/energy_step_size
-    e_delta = e_VB-e_CB
-    p_left = p_origin
-    p_right = p_left + width/total_width_step_size
-    p_delta = p_right-p_left
-    if CB==VB:
-        rect = patches.Rectangle((p_left, e_CB-0.005*size), p_delta, e_delta+0.01*size,
-                                linewidth=0, edgecolor='none', facecolor=color)
-    else:
-        rect = patches.Rectangle((p_left, e_CB), p_delta, e_delta,
-                                linewidth=0, edgecolor='none', facecolor=color)
+def create_energy_label(x_left, x_right, y, type, position, ax):
+    """Create and place the label for an energy level (in eV) of a layer
 
-    # Add the Energy label to the figure. For electrodes, only one label has to be added.
-    create_energy_label(p_left+0.01*size, p_right, e_CB, 0.05/energy_step_size, CB, size, ax)
-    if not CB == VB:
-        create_energy_label(p_left+0.01*size, p_right, e_VB, -0.12/energy_step_size, VB, size, ax)
+    Parameters
+    ----------
+    x_left : float
+        Left x position of the layer [m]
+    x_right : float
+        right x position of the layer [m]
+    y : float
+        Energy of the band [eV]
+    type : string
+        Type of band (CB, VB, Electrode)
+    position : float
+        Full length of the device
+    ax : axes
+        Axes object for the plot
+    """    
 
-    return (rect, p_right)
-
-def create_energy_label(x_left, x_right, y, offset, value, size, ax):
     # If the layer covers over 20% of the figure size, move the label to the x middle of the figure. Else align oit to the left side pf the layer.
-    if (x_right - x_left) > 0.2*size:
-        ax.text((x_left+x_right)/2,y+offset,value)
+    if 'VB' in type:
+        offset = 0.035
     else:
-        ax.text(x_left,y + offset,value)
+        offset = -0.01
+
+    if (x_right - x_left) > 0.2*position:
+        ax.text((x_left+x_right)/2,y+offset*y,y)
+    else:
+        ax.text(x_left,y+offset*y,y)
+
+def create_width_label(x_left,x_right,value,y_min,ax, color):
+    """ Create the label to displaythe width of a layer on the width bar
+
+    Parameters
+    ----------
+    x_left : float
+        Left x position of the layer [m]
+    x_right : float
+        right x position of the layer [m]
+    value : float
+        Width of the layer [m]
+    y_max : float
+        Lowest energy level of the device [eV]
+    ax : axes
+        Axes object for the plot
+    color : string
+        Color of the label
+    """    
+    ax.text((x_left+x_right)/2,y_min-0.7,round(value*1e9),color=color)
+
+
+def plot_device_widths(ax, y_min, L, LLTL, LRTL, L_original):
+    """Plot a width bar below the band energy diagram with the thickness of each layer.
+
+    Parameters
+    ----------
+    ax : axes
+        Axes object for the plot
+    y_max : float
+        Lowest energy level [eV]
+    L : float
+        Full width of the device
+    LLTL : float
+        Width of the Left Transport Layer [m]
+    LRTL : float
+        Width of the Right Transport Layer
+    L_original : List
+        List with the layer widths before scaling
+    """    
+    # Horizontal line below the band diagram
+    ax.hlines(y_min-0.75,0,L,color='k')
+    # Small vertical line on each layer interface
+    ax.vlines([0,LLTL,L-LRTL,L],y_min-0.85,y_min-0.65,color='k')
+    # Label for each segment
+    create_width_label(0,LLTL-0.05*L,L_original[1],y_min,ax,'k')
+    create_width_label(LLTL,L-LRTL-0.05*L,L_original[0]-L_original[1]-L_original[2],y_min,ax,'k')
+    create_width_label(L-LRTL,L-0.05*L,L_original[2],y_min,ax,'k')
+    # Label for the unit [nm]
+    ax.text(1.05*L,y_min-0.7,'[nm]',color='k')
 
 def create_band_energy_diagram(param):
+    """Create and plot the band energy diagram for the device based on the input parameters
+
+    Parameters
+    ----------
+    param : dict
+        Dictionary with the relevant parameters from the device parameters file
+
+    Returns
+    -------
+    Figure
+        Figure object with the band energy diagram
+    """    
+
     fig, ax = plt.subplots()
-
-    size = 1000000  # set number of grid points
-    ax.set_xlim(-0.01*size, size+0.01*size)  # Add extra whitespace to x-axis
-    ax.set_ylim(-size-0.01*size, 0.01*size)  # Add extra whitespace to y-axis
-
+    print(param['L_LTL'])
     # Read parameters
     L = float(param["L"])
     LLTL = float(param["L_LTL"])
@@ -72,9 +109,11 @@ def create_band_energy_diagram(param):
     VBLTL = -float(param["VB_LTL"])
     VBRTL = -float(param["VB_RTL"])
 
-    energy = [CB, VB, WL, WR, CBLTL, CBRTL, VBLTL, VBRTL]
+    E_low = min([CB,VB,WL,WR,CBLTL,CBRTL,VBLTL,VBRTL])
 
-    # Set a threshold width for bands to remain visible. Threshold is 10% of the total device width
+    # Save original values for width bar
+    L_original = [L,LLTL,LRTL]
+    # Set a threshold width for bands to remain visible. Threshold is 10% of the total device width. Only when Transport Layer are defined. 
     if not LLTL == 0 and not LRTL == 0:
         if LLTL < 0.1*L:
             LLTL = 0.1*L
@@ -85,55 +124,37 @@ def create_band_energy_diagram(param):
             L_diff = 0.1*L-(L-LLTL-LRTL)
             LLTL = LLTL - L_diff*frac
             LRTL = LRTL - L_diff*(1/frac)
-
-    # Electrode width configuration
-    electrode_width_fraction = 0.1  # Added fraction of the total device width
-    electrode_width = electrode_width_fraction*L  # Set the width of a electrode
-
-    # Width configuration
-    total_width = L+2*electrode_width
-    total_width_step_size = total_width/size  # m/gridpoint
-
-    # Energy configuration
-    max_energy = max(energy)
-    min_energy = min(energy)
-    energy_step_size = abs((max_energy-min_energy))/size  # ev/gridpoint
-    # Offset in energy compared to 0eV to shift the band diagram in view.
-    energy_offset = max_energy
-
-    # Left Electrode
-    left_electrode, p_origin_ltl = create_rectangle_patch(
-        WL, WL, energy_offset, energy_step_size, 0, electrode_width_fraction*size*total_width_step_size, total_width_step_size, 'k', size, ax)
-    ax.add_patch(left_electrode)
-
+    
     # Left Transport Layer
-    if LLTL != 0:
-        left_transport_layer, p_origin_layer = create_rectangle_patch(
-            CBLTL, VBLTL, energy_offset,  energy_step_size, p_origin_ltl, LLTL, total_width_step_size, '#AF312E', size, ax)
-        ax.add_patch(left_transport_layer)
-    else:
-        p_origin_layer = p_origin_ltl
+    if LLTL > 0:
+        ax.fill_between([0, LLTL],[CBLTL,CBLTL],y2=[VBLTL,VBLTL],color='#AF312E')
+        create_energy_label(0,LLTL,CBLTL,'CBLTL',L,ax)
+        create_energy_label(0,LLTL,VBLTL,'VBLTL',L,ax)
 
     # Layer 1
-    layer_1, p_origin_rtl = create_rectangle_patch(
-        CB, VB, energy_offset, energy_step_size, p_origin_layer, L-LLTL-LRTL, total_width_step_size, '#C7D5A0', size, ax)
-    ax.add_patch(layer_1)
+    ax.fill_between([LLTL, L-LRTL],[CB,CB],y2=[VB,VB],color='#C7D5A0')
+    create_energy_label(LLTL,L-LRTL,CB,'CB',L,ax)
+    create_energy_label(LLTL,L-LRTL,VB,'VB',L,ax)
 
     # Right Transport Layer
-    if LRTL != 0:
-        right_transport_layer, p_origin_right_electrode = create_rectangle_patch(
-            CBRTL, VBRTL, energy_offset, energy_step_size, p_origin_rtl, LRTL, total_width_step_size, '#95B2DA', size, ax)
-        ax.add_patch(right_transport_layer)
-    else:
-        p_origin_right_electrode = p_origin_rtl
+    if LRTL > 0:
+        ax.fill_between([L-LRTL,L],[CBRTL,CBRTL],y2=[VBRTL,VBRTL],color='#95B2DA')
+        create_energy_label(L-LRTL,L,CBRTL,'CBRTL',L,ax)
+        create_energy_label(L-LRTL,L,VBRTL,'VBRTL',L,ax)
+
+    # Left Electrode
+    ax.plot([-0.1*L,0],[WL,WL],color='k')
+    create_energy_label(-0.1*L,0,WL,'WL',L,ax)
+
     # Right Electrode
-    right_electrode, p_end = create_rectangle_patch(
-        WR, WR, energy_offset, energy_step_size, p_origin_right_electrode, electrode_width_fraction*size*total_width_step_size, total_width_step_size, 'k', size, ax)
-    ax.add_patch(right_electrode)
+    ax.plot([L,L+0.1*L],[WR,WR],color='k')    
+    create_energy_label(L,L+0.1*L,WR,'WR',L,ax)
 
     ax.axis('off')
+
+    plot_device_widths(ax, E_low, L, LLTL, LRTL, L_original)
+
     # plt.show()
-    # plt.savefig('./figures/band_diagram.png')
     return fig
 
 if __name__ == '__main__':
